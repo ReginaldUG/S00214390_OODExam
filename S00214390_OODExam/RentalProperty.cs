0009284e2f1ae588aa8694672b625e0a8da98d93
ReginaldUG <S00214390@mail.itsligo.ie>
2022-05-17 09:43:54 +0100

    RentalType class created and properties added

---
+++ S00214390_OODExam/RentalProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00214390_OODExam
+{
+    public class RentalProperty
+    {
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public enum RentalType
+        {
+            House,
+            Flat,
+            Share,
+
+        }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
